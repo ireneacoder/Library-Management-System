db016cef0067cedf679741351e61c097799071c5
ireneacoder <63922114+ireneacoder@users.noreply.github.com>
2023-08-13 20:33:44 +0530

    update5

--- src/Members/Members.js
+++ src/Members/Members.js
@@ -1,36 +1,59 @@
-
-import './Members.css'
+import "./Members.css";
 import { FiSearch } from "react-icons/fi";
-import Button from '../button/button';
-import Membercard from '../membercard/membercard';
+import Button from "../button/button";
+import Membercard from "../membercard/membercard";
 import { AiOutlinePlus } from "react-icons/ai";
-import { useState } from 'react';
-import Addmemberpopup from '../addmemberpopup/addmemberpopup';
-import React from 'react';
+import { useState, useEffect } from "react";
+import Addmemberpopup from "../addmemberpopup/addmemberpopup";
+import React from "react";
+import axios from "axios";
 
-export default function Members(props){
-    const [add, setadd] = useState(false);
+export default function Members(props) {
+  const [add, setadd] = useState(false);
 
-        const closeDialog = () => {
-            setadd(false);
-        }
-    return(
-        <>
-            {add && <Addmemberpopup imgurl={props.imgurl} closeDialog={closeDialog} />}
+  const closeDialog = () => {
+    setadd(false);
+  };
+  const [myData, setMyData] = useState(false);
+  const getApiData = async () => {
+    const res = await axios.get("http://127.0.0.1:5000/getMembers");
+    console.log(res.data);
+    setMyData(res.data);
+  };
+  useEffect(() => {
+    getApiData();
+  }, []);
 
-            <div className='horizontal-flex'>
-                <div className='searchbar'>
-                    <FiSearch size={25}/>
-                    <p className='search-text'> Search for Members</p>      
-                </div>
-            <Button buttonname='Search'/>
-            </div>
+  return (
+    <>
+      {add && (
+        <Addmemberpopup imgurl={props.imgurl} closeDialog={closeDialog} />
+      )}
 
-            <button className='addmember' onClick={setadd}><AiOutlinePlus size={30}/> Add Member</button>
+      <div className="horizontal-flex">
+        <div className="searchbar">
+          <FiSearch size={25} />
+          <p className="search-text"> Search for Members</p>
+        </div>
+        <Button buttonname="Search" />
+      </div>
 
-           <div className='userdetails'>
+      <button className="addmember" onClick={setadd}>
+        <AiOutlinePlus size={30} /> Add Member
+      </button>
 
-                <Membercard
+      <div className="userdetails">
+        {myData &&
+          myData.map((item) => {
+            return (
+              <Membercard
+                key={item._id}
+                username={item["name"]}
+                depth={item["debt"]}
+              />
+            );
+          })}
+        {/* <Membercard
                 username='Prerna Verma'
                 bookcount='02'
                 depth='400'/>
@@ -58,11 +81,8 @@ export default function Members(props){
                 <Membercard
                 username='Prerna Verma'
                 bookcount='02'
-                depth='400'/>
-
-           </div>
-
-        </>
-
-    )
+                depth='400'/> */}
+      </div>
+    </>
+  );
 }
--- src/addmemberpopup/addmemberpopup.js
+++ src/addmemberpopup/addmemberpopup.js
@@ -1,68 +1,66 @@
-
-import { useState } from 'react'
+import { useState } from "react";
 import { FiX } from "react-icons/fi";
 import { TextField, InputAdornment } from "@mui/material";
-import Button from '../button/button';
-import React from 'react';
-
+import React from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 const Addmemberpopup = (props) => {
-    return(
+  const [open, setOpen] = useState(false);
+  const config = {
+    headers: { "content-type": "multipart/form-data" },
+  };
+  const { register, handleSubmit } = useForm();
+  function addmember(data) {
+    let formData = new FormData();
+    formData.append("name", "lily");
+    formData.append("address", "xyz");
+    formData.append("dob", "0002-02-02");
+    axios.post("http://127.0.0.1:5000/addMember", { body: { data } });
+  }
+  return (
+    <div className="box">
+      <div className="dialog-wrapper" onClick={props.closeDialog}></div>
+      <div className="dialog-container">
+        <form onSubmit={handleSubmit(addmember)} className="inner-container">
+          <p className="poptitle"> Add New Member </p>
 
-        <div className='box'>
+          <div className="textboxes">
+            <TextField
+              {...register("name")}
+              size="medium"
+              sx={{ width: "36vw" }}
+              label="Name"
+              id="outlined-start-adornment"
+              variant="outlined"
+            />
 
-            <div className="dialog-wrapper" onClick={props.closeDialog}></div>
-            <div className="dialog-container">
-                <div className="inner-container">
-                    <p className='poptitle'> Add New Member </p>
+            <TextField
+              {...register("address")}
+              size="medium"
+              sx={{ width: "36vw" }}
+              label="Address"
+              id="outlined-start-adornment"
+              variant="outlined"
+            />
 
-                    <div className='textboxes'>
+            <TextField
+              {...register("dob")}
+              size="medium"
+              sx={{ width: "36vw" }}
+              label="DOB"
+              id="outlined-start-adornment"
+              variant="outlined"
+            />
+          </div>
+          <button className="button">Add Member</button>
 
-                        <TextField
-                            size="medium"
-                            sx={{width: '36vw'}}
-                            label="Name"
-                            id="outlined-start-adornment"
-                            variant="outlined"
-                        />
-
-                        <TextField
-                            size="medium"
-                            sx={{width: '36vw'}}
-                            label="Address"
-                            id="outlined-start-adornment"
-                            variant="outlined"
-                        />
-
-                        <TextField
-                            size="medium"
-                            sx={{width: '36vw'}}
-                            label="DOB"
-                            id="outlined-start-adornment"
-                            variant="outlined"
-                        />
-
-
-                    </div>
-
-                    <Button buttonname='Add Member'/>
-
-                    <div className="icon-container" onClick={props.closeDialog}>
-
-                        <FiX className='closeBtn' size={45}/>
-                    </div>
-
-                   
-                </div>
-
-
-            </div>
-
-
-
-
-
-        </div>
-    )
-}
-export default Addmemberpopup
+          <div className="icon-container" onClick={props.closeDialog}>
+            <FiX className="closeBtn" size={45} />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Addmemberpopup;
